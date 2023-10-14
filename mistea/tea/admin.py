d1a670bad5e3d0b7e7c31a595493f1ca39852d9d
tsukerin <125801694+setusq@users.noreply.github.com>
2023-10-14 18:06:42 +0300

    заполнены базы данных чаев и категорий

--- mistea/tea/admin.py
+++ mistea/tea/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import TeaCategory, Subscription, Tea
 
-# Register your models here.
+class TeaCategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(TeaCategory, TeaCategoryAdmin)
+
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    filter_horizontal = ('teas',)  # Добавляем возможность выбора чаев в подписке
+admin.site.register(Subscription, SubscriptionAdmin)
+
+class TeaAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'price', 'stock', 'available', 'created', 'updated']
+    list_filter = ['available', 'created', 'updated', 'category']
+    list_editable = ['price', 'stock', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+admin.site.register(Tea, TeaAdmin)
--- mistea/tea/models.py
+++ mistea/tea/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+class TeaCategory(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Категория чая'
+        verbose_name_plural = 'Категории чая'
+
+    def __str__(self):
+        return self.name
+
+class Subscription(models.Model):
+    name = models.CharField(max_length=200, db_index=True)
+    description = models.TextField(blank=True)
+    
+    # Связь с чаями, входящими в подписку
+    teas = models.ManyToManyField('tea', related_name='subscriptions', blank=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name = 'Подписка на чай'
+        verbose_name_plural = 'Подписки на чай'
+
+    def __str__(self):
+        return self.name
+
+class Tea(models.Model):
+    category = models.ForeignKey(TeaCategory, related_name='Чаи', on_delete=models.CASCADE)
+    name = models.CharField(max_length=200, db_index=True)
+    slug = models.SlugField(max_length=200, db_index=True)
+    image = models.ImageField(upload_to='teas/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('name',)
+        index_together = (('id', 'slug'),)
+
+    def __str__(self):
+        return self.name
