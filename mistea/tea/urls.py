1a5913b40438783b30d2b84c76f7128d104e7557
tsukerin <125801694+setusq@users.noreply.github.com>
2023-10-22 21:29:49 +0300

    добавлен пробный index.html
    для index.html style1.css

--- mistea/tea/urls.py
+++ mistea/tea/urls.py
@@ -3,7 +3,8 @@ from .views import *
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', home, name='home'),
+    # path('', home, name='home'),
+    path('', index, name='index'),
     path('tea-list/', tea_list, name='tea-list'),
     path('about/', TemplateView.as_view(template_name='tea/about.html'), name='about'),
     path('subs/', subs, name='subs'),
--- mistea/tea/views.py
+++ mistea/tea/views.py
@@ -2,8 +2,17 @@ from django.shortcuts import render, get_object_or_404
 from .models import TeaCategory, Tea
 from django.shortcuts import render
 
-def home(request):
-    return render(request, 'tea/home.html')
+# def home(request):
+#     context = {
+#         'active_page': 'home',
+#     }
+#     return render(request, 'tea/home.html', context)
+
+def index(request):
+    context = {
+        'active_page': 'index',
+    }
+    return render(request, 'tea/index.html', context)
     
 def tea_list(request):
     category = None
@@ -14,8 +23,12 @@ def tea_list(request):
     #     teas = teas.filter(category=category)
     return render(request, 'tea/tea_list.html', {'category': category, 'categories': categories, 'teas': teas})
 
+    
 def subs(request):
-    return render(request, 'tea/subs.html')
+    context = {
+        'active_page': 'subs',
+    }
+    return render(request, 'tea/subs.html', context)
 
 def tea_detail(request, id, slug):
     tea = get_object_or_404(Tea, id=id, slug=slug, available=True)
