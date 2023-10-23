254b2de5d42c33bc04cb8caae1851be239fbfcaf
tsukerin <125801694+setusq@users.noreply.github.com>
2023-10-23 11:25:24 +0300

    d
    варианты html страницы

--- mistea/tea/urls.py
+++ mistea/tea/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('tea-list/', tea_list, name='tea-list'),
     path('about/', TemplateView.as_view(template_name='tea/about.html'), name='about'),
     path('subs/', subs, name='subs'),
+    path('home/', subs, name='home'),
     path('tea-detail/<int:pk>/', tea_detail, name='tea-detail'), 
 ]
