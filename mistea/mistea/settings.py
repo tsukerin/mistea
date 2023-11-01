f0f1bbd2db2ca3f5894d7d02657ff64677b0376c
setusq <setusq@gmail.com>
2023-11-02 00:50:28 +0300

    allowed hosts

--- mistea/mistea/settings.py
+++ mistea/mistea/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9zauclxa!+3077b59t$#ova5ocdiwi*79f7%qautmnb!50p#di
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
